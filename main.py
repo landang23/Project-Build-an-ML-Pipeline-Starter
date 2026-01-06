41f5fd2db2e7c436c535a9faed6843a404cbdbb6
landang23 <36575597+landang23@users.noreply.github.com>
2026-01-06 12:32:45 -0700

    Added code for testing model with production alias

--- main.py
+++ main.py
@@ -120,11 +120,13 @@ def go(config: DictConfig):
             pass
 
         if "test_regression_model" in active_steps:
-
-            ##################
-            # Implement here #
-            ##################
-
+            _ = mlflow.run(
+                uri=os.path.join(hydra.utils.get_original_cwd(), "components", "test_regression_model"),
+                parameters={
+                    "mlflow_model": "random_forest_export:prod",
+                    "test_dataset": "test_data.csv:latest"
+                },
+            )
             pass
 
 
